9bcb447417ee4d7f4a715a23629f25fcf2a5621e
Bertrand C <bertrand.ciroux@gmail.com>
2018-12-10 22:47:16 +0100

    Fix: Button class specification and implementation improved

--- src/Button.h
+++ src/Button.h
@@ -10,27 +10,9 @@ class Button
     uint8_t _pin;
 
   public:
-    Button(uint8_t pin) : _pin(pin) {}
-
-    void begin()
-    {
-        pinMode(_pin, INPUT_PULLUP);
-        _state = digitalRead(_pin);
-    }
-
-    bool isReleased()
-    {
-        bool v = digitalRead(_pin);
-        if (v != _state)
-        {
-            _state = v;
-            if (_state)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
+    Button(uint8_t pin);
+    void begin();
+    bool isReleased();
 };
 
 #endif
