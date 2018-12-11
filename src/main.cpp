5e0949ec419998e67f868c1199331e943815f4ed
Bertrand C <bertrand.ciroux@gmail.com>
2018-12-11 23:07:41 +0100

    Update: starting refactoring to use DS3232RTC and OneButton libs

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,7 @@
-#include <Wire.h>
-#include <RtcDS3231.h>
-RtcDS3231<TwoWire> Rtc(Wire);
-
-// Local libs
-#include "Button.h"
-#include "DateTimeUtils.h"
-
-#define countof(a) (sizeof(a) / sizeof(a[0]))
+#include <Arduino.h>
+#include <DS3232RTC.h>
+#include <Streaming.h>
+#include <OneButton.h>
 
 // ----- PIN definition
 // Push buttons for clock setup
@@ -20,223 +15,145 @@ const int asleepLed = 7;
 // -----
 
 // ----- Global vars
-int mode = 0;           // running mode = 0, set year = 1, set month = 2, etc.
-bool sleepMode = false; // tells which led to turn on
-RtcDateTime setupDateTime;
 unsigned long displayTimer = 0; // currently used for debugging purpose
+unsigned int clockMode = 0;
+tmElements_t timeToSet;
 
 // ----- Buttons
-Button modeButton(MODE_SW_PIN);
-Button plusButton(PLUS_SW_PIN);
-Button minusButton(MINUS_SW_PIN);
+OneButton modeBtn(8, true);
+OneButton minusBtn(9, true);
+OneButton plusBtn(10, true);
 // -----
 
-void printToSerial(const RtcDateTime &dt)
-{
-    if (millis() >= displayTimer)
-    {
-        Serial.print("Mode: ");
-        Serial.print(mode);
-        Serial.print(" ");
-        Serial.print("SleepMode: ");
-        Serial.print(sleepMode);
-        Serial.print(" ");
-        Serial.print("WD: ");
-        char datestring[22];
-        snprintf_P(datestring,
-                   countof(datestring),
-                   PSTR("%1u %02u/%02u/%04u %02u:%02u:%02u"),
-                   dt.DayOfWeek(),
-                   dt.Day(),
-                   dt.Month(),
-                   dt.Year(),
-                   dt.Hour(),
-                   dt.Minute(),
-                   dt.Second());
-        Serial.println(datestring);
-        displayTimer = millis() + 1000;
-    }
-}
+// ----- Prototypes
+void printDateTime(time_t t);
+void printDateTime(tmElements_t t);
+void modeBtnClick();
+void minusBtnClick();
+void plusBtnClick();
+void displayClock();
+void incrementYear();
+void decrementYear();
+// -----
 
 void setup()
 {
+    // setup serial
     Serial.begin(9600);
-    Serial.print("compiled: ");
-    Serial.print(__DATE__);
-    Serial.println(__TIME__);
 
-    Rtc.Begin();
-    modeButton.begin();
-    plusButton.begin();
-    minusButton.begin();
-
-    RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
-    printToSerial(compiled);
-
-    if (!Rtc.IsDateTimeValid())
+    // init RTC if needed
+    if (RTC.oscStopped(false))
     {
-        Serial.println("RTC lost confidence in the DateTime!");
-        Rtc.SetDateTime(compiled);
+        setTime(0, 0, 0, 1, 12, 2018);
+        RTC.set(now());
     }
 
-    if (!Rtc.GetIsRunning())
-    {
-        Serial.println("RTC was not actively running, starting now");
-        Rtc.SetIsRunning(true);
-    }
-
-    setupDateTime = Rtc.GetDateTime();
-    if (setupDateTime < compiled)
-    {
-        Serial.println("RTC is older than compile time!  (Updating DateTime)");
-        Rtc.SetDateTime(compiled);
-    }
-    else if (setupDateTime > compiled)
-    {
-        Serial.println("RTC is newer than compile time. (this is expected)");
-    }
-    else if (setupDateTime == compiled)
-    {
-        Serial.println("RTC is the same as compile time! (not expected but all is fine)");
-    }
-
-    // never assume the Rtc was last configured by you, so
-    // just clear them to your needed state
-    Rtc.Enable32kHzPin(false);
-    Rtc.SetSquareWavePin(DS3231SquareWavePin_ModeNone);
-
-    // sleep / awake LED
-    pinMode(awakeLed, OUTPUT);
-    pinMode(asleepLed, OUTPUT);
-}
-
-void showCurrentTime()
-{
-    // will be changed to displaying to the LED segments
-}
-
-void setYear()
-{
-    int value = setupDateTime.Year();
-    value = plusButton.isReleased() ? value + 1 : minusButton.isReleased() ? value - 1 : value;
-    setupDateTime = RtcDateTime(value,
-                                setupDateTime.Month(),
-                                setupDateTime.Day(),
-                                setupDateTime.Hour(),
-                                setupDateTime.Minute(),
-                                setupDateTime.Second());
-}
-
-void setMonth()
-{
-    int value = setupDateTime.Month();
-    value = plusButton.isReleased() ? (value == 12 ? 1 : value + 1) : (minusButton.isReleased() ? value == 1 ? 12 : value - 1 : value);
-    setupDateTime = RtcDateTime(setupDateTime.Year(),
-                                value,
-                                setupDateTime.Day(),
-                                setupDateTime.Hour(),
-                                setupDateTime.Minute(),
-                                setupDateTime.Second());
-}
-
-void setDay()
-{
-    int value = setupDateTime.Day();
-    int nbDaysInMonth = daysInMonth(setupDateTime.Year(), setupDateTime.Month());
-    value = plusButton.isReleased() ? (value == nbDaysInMonth ? 1 : value + 1) : (minusButton.isReleased() ? (value == 1 ? nbDaysInMonth : value - 1) : value);
-    setupDateTime = RtcDateTime(setupDateTime.Year(),
-                                setupDateTime.Month(),
-                                value,
-                                setupDateTime.Hour(),
-                                setupDateTime.Minute(),
-                                setupDateTime.Second());
-}
-
-void setHour()
-{
-    int value = setupDateTime.Hour();
-    value = plusButton.isReleased() ? (value == 23 ? 0 : value + 1) : (minusButton.isReleased() ? (value == 0 ? 23 : value - 1) : value);
-    setupDateTime = RtcDateTime(setupDateTime.Year(),
-                                setupDateTime.Month(),
-                                setupDateTime.Day(),
-                                value,
-                                setupDateTime.Minute(),
-                                setupDateTime.Second());
-}
-void setMinute()
-{
-    int value = setupDateTime.Minute();
-    value = plusButton.isReleased() ? (value == 59 ? 0 : value + 1) : (minusButton.isReleased() ? (value == 0 ? 59 : value - 1) : value);
-    setupDateTime = RtcDateTime(setupDateTime.Year(),
-                                setupDateTime.Month(),
-                                setupDateTime.Day(),
-                                setupDateTime.Hour(),
-                                value,
-                                setupDateTime.Second());
-}
-
-void setDatetime()
-{
-    Rtc.SetDateTime(setupDateTime);
+    // setup button handler
+    modeBtn.attachClick(modeBtnClick);
+    minusBtn.attachClick(minusBtnClick);
+    plusBtn.attachClick(plusBtnClick);
 }
 
 void loop()
 {
-    RtcDateTime now = Rtc.GetDateTime();
-    // check if mode button is pressed
-    if (modeButton.isReleased())
-    {
-        mode += 1;
-    }
+    // read buttons
+    modeBtn.tick();
+    minusBtn.tick();
+    plusBtn.tick();
 
-    // which mode to apply?
-    switch (mode)
+    // update the timeToSet
+    RTC.read(timeToSet);
+
+    // manage clock mode
+    switch (clockMode)
     {
     case 0:
-        showCurrentTime();
+        displayClock();
         break;
     case 1:
-        setYear();
+        // setupYear();
         break;
     case 2:
-        setMonth();
+        // setupMonth();
         break;
     case 3:
-        setDay();
+        // setupDay();
         break;
     case 4:
-        setHour();
+        // setupHour();
         break;
     case 5:
-        setMinute();
+        // setupMinute();
         break;
     default:
-        setDatetime();
-        mode = 0;
+        RTC.write(timeToSet);
+        clockMode = 0;
         break;
     }
 
-    if (mode != 0)
+    if (millis() >= displayTimer)
     {
-        printToSerial(setupDateTime);
+        if (clockMode != 0)
+        {
+            printDateTime(timeToSet);
+        }
+        else
+        {
+            printDateTime(RTC.get());
+        }
+        displayTimer = millis() + 1000;
     }
-    else
-    {
-        printToSerial(now);
-    }
+}
 
-    // alarms
-    // weekdays 20:00-07:30
-    // weekends 20:00-08:00 and 13:00-15:00
-    if (now.DayOfWeek() == 0 || now.DayOfWeek() == 6)
-    {
-        sleepMode = !((now.Hour() >= 15 && now.Hour() < 20) || (now.Hour() >= 8 && now.Hour() < 13));
-    }
-    else
-    {
-        sleepMode = !((now.Hour() > 7 || (now.Hour() == 7 && now.Minute() >= 30)) && now.Hour() < 20);
-    }
+void modeBtnClick()
+{
+    clockMode++;
+    Serial << "ClockMode: " << clockMode << endl;
+}
 
-    digitalWrite(awakeLed, !sleepMode);
-    digitalWrite(asleepLed, sleepMode);
+void minusBtnClick()
+{
+    switch (clockMode)
+    {
+    case 1:
+        decrementYear();
+        break;
+    }
+}
+
+void plusBtnClick()
+{
+    switch (clockMode)
+    {
+    case 1:
+        incrementYear();
+        break;
+    }
+}
+
+void displayClock()
+{
+}
+
+void decrementYear()
+{
+    timeToSet.Year--;
+}
+void incrementYear()
+{
+    timeToSet.Year++;
+}
+
+void printDateTime(time_t t)
+{
+    Serial << ((day(t) < 10) ? "0" : "") << _DEC(day(t));
+    Serial << monthShortStr(month(t)) << _DEC(year(t)) << ' ';
+    Serial << ((hour(t) < 10) ? "0" : "") << _DEC(hour(t)) << ':';
+    Serial << ((minute(t) < 10) ? "0" : "") << _DEC(minute(t)) << ':';
+    Serial << ((second(t) < 10) ? "0" : "") << _DEC(second(t)) << endl;
+}
+
+void printDateTime(tmElements_t t)
+{
+    Serial << t.Day << "-" << t.Month << "-" << t.Year + 1970 << " " << t.Hour << ":" << t.Minute << ":" << t.Second << endl;
 }
